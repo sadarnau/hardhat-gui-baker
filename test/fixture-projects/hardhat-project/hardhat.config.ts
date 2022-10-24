491ceb6c39fcb4d7c709a4b3474607b7403c2c08
samphoto <samidarnaud@gmail.com>
2022-10-24 16:13:54 +0200

    feat(front): connector with wagmi

--- test/fixture-projects/hardhat-project/hardhat.config.ts
+++ test/fixture-projects/hardhat-project/hardhat.config.ts
@@ -4,7 +4,6 @@ import { HardhatUserConfig } from "hardhat/types";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
-import "../../../src/front";
 import "../../../src/index";
 
 const config: HardhatUserConfig = {
