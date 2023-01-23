01e77f74d5baad67b5c67203901861e215b6ca0a
samphoto <samidarnaud@gmail.com>
2023-01-23 21:55:04 +0100

    added next-website on packages

--- packages/contract-front/src/ContractContext.ts
+++
@@ -1,5 +0,0 @@
-import { ToChangeAbi } from "./ToChangeAbi";
-import { ToChangeType } from "./typechain-types/contracts/ToChangeType";
-
-export const ContractAbi = ToChangeAbi;
-export interface ContractType extends ToChangeType {}
--- packages/next-website/pages/ContractContext.ts
+++
@@ -1,5 +0,0 @@
-import { SamTokenAbi } from "./SamTokenAbi";
-import { SamToken } from "./typechain-types/contracts/SamToken";
-
-export const ContractAbi = SamTokenAbi;
-export interface ContractType extends SamToken {}
