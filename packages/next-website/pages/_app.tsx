1108a44a67327570c59e12e45c3ed2697b839c5c
samphoto <samidarnaud@gmail.com>
2023-01-24 00:03:40 +0100

    added pages for the website

--- packages/next-website/pages/_app.tsx
+++ packages/next-website/pages/_app.tsx
@@ -1,6 +1,67 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ConnectButton, connectorsForWallets } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import {
+  injectedWallet,
+  rainbowWallet,
+  walletConnectWallet,
+  ledgerWallet,
+  braveWallet,
+  metaMaskWallet,
+  coinbaseWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+
+const { chains, provider } = configureChains(
+  [mainnet, polygon, optimism, arbitrum],
+  [
+    // alchemyProvider({ apiKey: process.env.ALCHEMY_ID })
+    jsonRpcProvider({
+      rpc: () => ({
+        http: `http://127.0.0.1:8545/`,
+      }),
+    }),
+  ]
+);
+
+const connectors = connectorsForWallets([
+  {
+    groupName: "Recommended",
+    wallets: [
+      injectedWallet({ chains }),
+      metaMaskWallet({ chains }),
+      walletConnectWallet({ chains }),
+    ],
+  },
+  {
+    groupName: "Other",
+    wallets: [
+      rainbowWallet({ chains }),
+      coinbaseWallet({ appName: "Frontify", chains }),
+      braveWallet({ chains }),
+      ledgerWallet({ chains }),
+    ],
+  },
+]);
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />;
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
--- packages/next-website/pages/index.tsx
+++ packages/next-website/pages/index.tsx
@@ -1,3 +1,30 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import FunctionConstant from "./FunctionConstant";
+import { ethers, Contract } from "ethers";
+import Function from "./Function";
+import { ContractAbi, ContractType } from "./ContractContext";
+
+function FList({ contract }: { contract: Contract }) {
+  return Object.keys(contract.interface.functions).map((arg) => {
+    if (contract.interface.functions[arg].constant)
+      return (
+        <FunctionConstant key={arg} functionName={arg} contract={contract} />
+      );
+    else return <Function key={arg} functionName={arg} contract={contract} />;
+  });
+}
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const deployedCoin = new ethers.Contract(
+    "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    ContractAbi
+  ) as ContractType;
+  return (
+    <div>
+      <ConnectButton />
+      <div>
+        <FList contract={deployedCoin}></FList>
+      </div>
+    </div>
+  );
 }
