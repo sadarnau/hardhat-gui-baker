67942feb42d17a81bea4a3143cd6132ef477318a
gdupont <gdupont.pro@gmail.com>
2023-01-30 15:12:22 +0100

    feat: write and constant toggle

--- packages/next-website/pages/index.tsx
+++ packages/next-website/pages/index.tsx
@@ -3,29 +3,75 @@ import FunctionConstant from "./FunctionConstant";
 import { ethers, Contract } from "ethers";
 import Function from "./Function";
 import { ContractAbi, ContractType } from "./ContractContext";
+import { useState } from "react";
 
-function FList({ contract }: { contract: Contract }) {
-  return Object.keys(contract.interface.functions).map((arg) => {
-    if (contract.interface.functions[arg].constant)
-      return (
-        <FunctionConstant key={arg} functionName={arg} contract={contract} />
-      );
-    return <Function key={arg} functionName={arg} contract={contract} />;
+function FList({
+  contract,
+  displayConstant,
+  displayWrite,
+}: {
+  contract: Contract;
+  displayConstant: boolean;
+  displayWrite: boolean;
+}) {
+  const writeFx = Object.keys(contract.interface.functions).filter((arg) => {
+    return !contract.interface.functions[arg].constant;
   });
+
+  const constFx = Object.keys(contract.interface.functions).filter((arg) => {
+    return contract.interface.functions[arg].constant;
+  });
+
+  const writeComponents = displayWrite
+    ? writeFx.map((arg) => (
+        <Function key={arg} functionName={arg} contract={contract} />
+      ))
+    : [];
+  const constComponents = displayConstant
+    ? constFx.map((arg) => (
+        <FunctionConstant key={arg} functionName={arg} contract={contract} />
+      ))
+    : [];
+
+  if (writeComponents.length > 0)
+    return writeComponents.concat(constComponents);
+  return constComponents;
 }
 
 export default function Home() {
+  const [displayConstant, setDisplayConstant] = useState(true);
+  const [displayWrite, setDisplayWrite] = useState(true);
   const deployedCoin = new ethers.Contract(
     "0x5FbDB2315678afecb367f032d93F642f64180aa3",
     ContractAbi
   ) as ContractType;
   return (
     <div className=" m-5">
-      <div className="mt-5">
+      <div className="mt-5 left-0">
         <ConnectButton />
       </div>
       <div>
-        <FList contract={deployedCoin}></FList>
+        <button
+          onClick={() => setDisplayConstant(!displayConstant)}
+          className={`mr-2 min-h-8 btn btn-outline input-xs input-primary border-2 h-6 ${
+            displayConstant ? "input-info" : "input-secondary bg-secondary"
+          }`}
+        >
+          Constant Functions
+        </button>
+        <button
+          onClick={() => setDisplayWrite(!displayWrite)}
+          className={`m-2 min-h-8 btn btn-outline input-xs border-2 h-6 ${
+            displayWrite ? "input-info" : "input-secondary bg-secondary"
+          }`}
+        >
+          Write Functions
+        </button>
+        <FList
+          contract={deployedCoin}
+          displayConstant={displayConstant}
+          displayWrite={displayWrite}
+        ></FList>
       </div>
     </div>
   );
